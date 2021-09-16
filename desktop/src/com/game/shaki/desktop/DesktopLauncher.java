5266ef64692e359f5ba7cfea0a225b36441732dc
nhattan22399 <linhatan99@gmail.com>
2021-09-16 09:53:17 +0700

    first commit

---
+++ desktop/src/com/game/shaki/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.game.shaki.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.game.shaki.CanyonBunnyMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
