1011d0584b742539e08ef606d5c8d8ef246a7a5f
nhattan22399 <linhatan99@gmail.com>
2021-09-16 10:39:56 +0700

    Thiet lap game canyon bunny

---
+++ core/src/com/game/shaki/WorldController.java
@@ -0,0 +1,9 @@
+package com.game.shaki;
+
+public class WorldController {
+	 private static final String TAG =
+		        WorldController.class.getName();
+    public WorldController() {}
+    private void init() {}
+    public void update(float deltaTime) {}
+}
